acf91a791a39c138dafe89d49bd1e7b7f4249c96
WmW <zhouzheng0646@qq.com>
2023-06-18 17:55:50 +0800

    2023-06-18

--- lerna-discrte/app/tableList.tsx
+++ lerna-discrte/app/tableList.tsx
@@ -1,42 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Table } from 'antd';
+import { get_set } from 'discrete-core';
 
 function TableList() {
-  const dataSource = [
-    {
-      key: '1',
-      name: '胡彦斌',
-      age: 32,
-      address: '西湖区湖底公园1号',
-    },
-    {
-      key: '2',
-      name: '胡彦祖',
-      age: 42,
-      address: '西湖区湖底公园1号',
-    },
-  ];
-
-  const columns = [
-    {
-      title: '姓名',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: '年龄',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: '住址',
-      dataIndex: 'address',
-      key: 'address',
-    },
-  ];
+  const aa = get_set('a');
 
   return (
-    <Table dataSource={dataSource} columns={columns} />
+    <div>23</div>
   );
 }
 
